1cfb281e5c052e009edf0ed7afe6f9677f80c36d
noam.feldman <noam.feldman@global-e.com>
2023-01-05 18:06:16 +0200

    Add signin and signup logic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -15,14 +15,20 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const onSignedOUtHandler = () => {
+    setIsSignedIn(false);
+  }
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
+          <Header isSignedIn={isSignedIn} onSignOut={onSignedOUtHandler}/>
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path="/auth" component={AuthLazy} />
+              <Route path="/auth" >
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
