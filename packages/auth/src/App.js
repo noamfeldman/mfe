10ede771b49e764e984a7f10d6910bdca509d78b
noam.feldman <noam.feldman@global-e.com>
2023-01-05 16:51:42 +0200

    Auth

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -13,6 +13,7 @@ const generateClassName = createGenerateClassName({
 });
 
 export default ({ history }) => {
+  console.log('delete me!');
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
